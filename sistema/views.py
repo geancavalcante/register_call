269424dc3f1f2f8b0a836ff3110960a5973b366b
geancavalcante <geanfeitosacavalcanteof@gmail.com>
2025-09-24 13:01:53 -0300

    five commit

--- sistema/views.py
+++ sistema/views.py
@@ -21,11 +21,11 @@ def index(request):
             senha = request.POST.get("senha"),
         )
 
-        return render(request, "static/index.html")
+        return render(request, "index.html")
 
     
     else:
-        return render(request, "static/index.html")
+        return render(request, "index.html")
 
 
       
