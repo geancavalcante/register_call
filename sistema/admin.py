b46c47a5e914f6c0fd6cd7d26df0d5008382e54d
geancavalcante <geanfeitosacavalcanteof@gmail.com>
2025-10-13 03:30:25 -0300

    opaaa

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Chamados
+from .models import Chamados, Chamados_planejados
 
 admin.site.register(Chamados)
+admin.site.register(Chamados_planejados)
