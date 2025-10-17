5d7d87c517d15e75507c56e8751e8af2d8f2ab15
geancavalcante <geanfeitosacavalcanteof@gmail.com>
2025-10-17 13:09:48 -0300

    resolver bug

--- source/settings.py
+++ source/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h-uno6pnj1(*wthrw7lbv!+r*ax7fuie$sge@fynde-#v(q$17
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['161.97.95.90', 'localhost', '127.0.0.1']
 
 
 # Application definition
